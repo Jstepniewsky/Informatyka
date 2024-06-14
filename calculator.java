1de551909f926fd5f45bd25e081dba475cc4a6d4
Jstepniewsky <kubbonekk@gmail.com>
2024-06-14 23:44:27 +0200

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Prosty kalkulator obslugujacy operacje +, -, *, /");
+        
+
+        System.out.print("Podaj pierwszą liczbę: ");
+        double pierwszaLiczba = scanner.nextDouble();
+
+        System.out.print("Podaj symbol operacji (+, -, *, /): ");
+        char operacja = scanner.next().charAt(0);
+        
+        System.out.print("Podaj drugą liczbę: ");
+        double drugaLiczba = scanner.nextDouble();
+        
+        double wynik = 0;
+        boolean poprawnaOperacja = true;
+        
+        switch (operacja) {
+            case '+':
+                wynik = pierwszaLiczba + drugaLiczba;
+                break;
+            case '-':
+                wynik = pierwszaLiczba - drugaLiczba;
+                break;
+            case '*':
+                wynik = pierwszaLiczba * drugaLiczba;
+                break;
+            case '/':
+                if (drugaLiczba != 0) {
+                    wynik = pierwszaLiczba / drugaLiczba;
+                } else {
+                    System.out.println("Błąd: dzielenie przez zero!");
+                    poprawnaOperacja = false;
+                }
+                break;
+            default:
+                System.out.println("Błąd: nieprawidłowy symbol operacji!");
+                poprawnaOperacja = false;
+        }
+        
+        if (poprawnaOperacja) {
+            System.out.println("Wynik: " + wynik);
+        }
+ 
+        System.out.println("Naciśnij Enter, aby zakończyć...");
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+
+        scanner.close();
+    }
+}
